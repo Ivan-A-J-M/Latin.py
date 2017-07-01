7d3d2be61851a04f3511de948af4ff6ed4d8fa75
Andrew Melck <andrew.melck@commerceguys.com>
2017-07-01 11:29:26 +0200

    First commit

---
+++ Latin.py
@@ -0,0 +1,19 @@
+indexval = 0
+latin_vocab = ["templum","Tempel","basilica","Basilika","circus","Zirkus","senator","Senator","forum","Marktplatz Öffentlichkeit",
+"properare","eilen","nam","denn","ibid","ort","curia","Kurie",
+"esse","sein","hic","hier","turba","Menschenmenge Verwirrung",
+"stare","stehen","et","und","clamare","laut rufen",
+"ave","sei gegrüßt","gaudere","sich freuen",
+"salve","sei gegrüßt","salvete","seid gegrüßt","subito","plötzlich","servus","Sklave","adesse","da sein","rogare","bitten fragen","ubi","wo","ridere","lachen","tum","da darauf dann","hodie","heute","ludus","Spiel Schule","sed","aber",
+"amicus","Freund","diu","lange Zeit","exspectare","warten",
+"tandem","endlich","populus","Volk","etiam","auch","nunc","jetzt","tacere","schweigen","porta","Tor","patere","offenstehen",
+"equus","Pferd","accedere","herbeikommen","denique","schließlich",
+"signum","Zeichen","dare","geben","currere","eilen",
+"surgere","aufstehen aufrichten","victor","Sieger","ecce","schau/schaut","praemium","Belohnung (Sieges-) Preis","adan","nach zu"]
+#a = input ("Hello!")
+#if a =="hi":
+#	print ("how are you?")\
+for i in range(len(latin_vocab)):     
+	print(latin_vocab[indexval])
+	indexval += 1
+
